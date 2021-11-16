9cb3d5217015b855637108f0f8b65bc8ad932100
Petter <petterfogel96@gmail.com>
2021-11-16 16:11:16 +0100

    Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { FC } from "react";
+import Header from "./components/layout/header/Header";
 
 const App: FC = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;

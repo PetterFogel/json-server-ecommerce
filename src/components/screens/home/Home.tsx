9318d7ab756c6e9ce697b35f8bb9bc6929609415
Petter <petterfogel96@gmail.com>
2021-11-18 18:00:28 +0100

    Add content for home and some styling.

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from "react";
+import classes from "./Home.module.css";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <section className={classes.section}>
+      <div className={classes.homeContainer}>
+        <div className={classes.introHolder}>
+          <h2 className={classes.pageTitle}>Home</h2>
+          <p className={classes.pageSubtitle}>lorem ipsum</p>
+          <button className={classes.pageBtn}>Products</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;

736e20b65f44d2bbf272b5d1b961b332b465811f
Petter <petterfogel96@gmail.com>
2021-11-18 17:59:14 +0100

    Add Link from react-router-dom for logo and list items.

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,13 +1,24 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import classes from "./Header.module.css";
 
 const Header: FC = () => {
   return (
     <header className={classes.header}>
-      <div>Logo</div>
+      <Link to="/" className={classes.logo}>
+        <div>Poster</div>
+      </Link>
       <ul className={classes.ul}>
-        <li>Products</li>
-        <li>About</li>
+        <li>
+          <Link to="/products" className={classes.link}>
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" className={classes.link}>
+            About
+          </Link>
+        </li>
       </ul>
     </header>
   );

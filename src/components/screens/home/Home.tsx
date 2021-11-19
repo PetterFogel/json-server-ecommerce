f17494d51e13ab45c0ea48830b22f0dd1b1ba3c1
Petter <petterfogel96@gmail.com>
2021-11-19 15:50:29 +0100

    Add useNavigation hook for button.

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./Home.module.css";
 
 const Home: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <section className={classes.section}>
       <div className={classes.homeContainer}>
         <div className={classes.introHolder}>
           <h2 className={classes.pageTitle}>Home</h2>
           <p className={classes.pageSubtitle}>lorem ipsum</p>
-          <button className={classes.pageBtn}>Products</button>
+          <button
+            className={classes.pageBtn}
+            onClick={() => navigate("/products")}
+          >
+            Products
+          </button>
         </div>
       </div>
     </section>

b566386b3c08247571ca9e1a620f542b1b72c1b2
Petter <petterfogel96@gmail.com>
2021-11-19 15:52:10 +0100

    Add fetch to get the poster data and render it in the ui.

--- src/components/screens/products/Products.tsx
+++ src/components/screens/products/Products.tsx
@@ -1,7 +1,38 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { Product } from "../../../models/product";
+import classes from "./Products.module.css";
 
 const Products: FC = () => {
-  return <div>Products</div>;
+  const [data, setData] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/posters")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error, "Error!"));
+  }, []);
+
+  return (
+    <section className={classes.section}>
+      <h2 className={classes.sectionTitle}>Products</h2>
+      <div className={classes.productContainer}>
+        {data.map((product) => (
+          <div key={product.id} className={classes.productHolder}>
+            <div className={classes.imageHolder}>
+              <img
+                src={product.url}
+                alt="product"
+                className={classes.imageStyle}
+              />
+            </div>
+            <h4 className={classes.productTitle}>{product.title}</h4>
+            {/* <p className={classes.productTitle}>{product.description}</p>
+            <p className={classes.productTitle}>{product.category}</p> */}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Products;

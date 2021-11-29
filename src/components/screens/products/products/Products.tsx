55bd58467d61ef636c5a8b9dacaaf28c2716608e
Petter <petterfogel96@gmail.com>
2021-11-29 18:42:49 +0100

    Add product to new folder and import productItem to render each data Item.

---
+++ src/components/screens/products/products/Products.tsx
@@ -0,0 +1,31 @@
+import { FC, useContext, useEffect } from "react";
+import classes from "./Products.module.css";
+import Context from "../../../../context/Context";
+import ProductItem from "../productItem/ProductItem";
+
+const Products: FC = () => {
+  const { data, isLoading, error, sendRequest } = useContext(Context);
+
+  useEffect(() => {
+    sendRequest("http://localhost:3000/posters");
+  }, []);
+
+  if (error) return <p>Oops! Something went wrong...</p>;
+
+  return (
+    <section className={classes.section}>
+      <h2 className={classes.sectionTitle}>Products</h2>
+      {isLoading ? (
+        <p className={classes.loading}>Loading...</p>
+      ) : (
+        <div className={classes.productContainer}>
+          {data.map((poster) => (
+            <ProductItem key={poster.id} poster={poster} />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default Products;

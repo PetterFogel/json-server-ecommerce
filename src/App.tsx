6fa4d1b1d69cfe76ccb1e1e983d63e91db20e0ea
Petter <petterfogel96@gmail.com>
2021-12-13 19:31:16 +0100

    Add conditional rendering for sideCart.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import Header from "./components/layout/header/Header";
 import Pages from "./components/layout/pages/Pages";
 import "./App.css";
+import Context from "./context/Context";
+import SideCart from "./components/layout/cart/sideCart/SideCart";
 
 const App: FC = () => {
+  const { isCartOpen } = useContext(Context);
+
   return (
     <>
       <Header />
       <main>
         <Pages />
+        {isCartOpen && <SideCart />}
       </main>
     </>
   );

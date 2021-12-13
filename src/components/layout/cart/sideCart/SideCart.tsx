1d971bed527bf500b044ba20558d7ca659fc53a1
Petter <petterfogel96@gmail.com>
2021-12-13 19:41:09 +0100

    Add logic to render items in sideCart.

--- src/components/layout/cart/sideCart/SideCart.tsx
+++ src/components/layout/cart/sideCart/SideCart.tsx
@@ -4,12 +4,25 @@ import { BsXLg } from "react-icons/bs";
 import Context from "../../../../context/Context";
 
 const SideCart: FC = () => {
-  const { openCart } = useContext(Context);
+  const { cart, openCart } = useContext(Context);
 
   return (
     <div className={classes.CartContainer}>
       <BsXLg className={classes.CloseIcon} onClick={openCart} />
       <h3 className={classes.CartTile}>Your Cart</h3>
+      <div>
+        {cart.map((item) => (
+          <div>
+            <div>
+              <img src={item.url} alt="poster" />
+            </div>
+            <div>
+              <h4>{item.title}</h4>
+              <p>{item.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

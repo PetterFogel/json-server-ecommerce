4c77c1a3d02639cdb614b60e0c24ad7050d8f85e
Petter <petterfogel96@gmail.com>
2022-01-13 18:41:11 +0100

    Add content with total amout title and buttons.

--- src/components/layout/cart/sideCart/SideCart.tsx
+++ src/components/layout/cart/sideCart/SideCart.tsx
@@ -5,16 +5,23 @@ import Context from "../../../../context/Context";
 import CartItem from "../cartItem/CartItem";
 
 const SideCart: FC = () => {
-  const { cart, openCart } = useContext(Context);
+  const { cart, openCart, emptyCart } = useContext(Context);
 
   return (
     <div className={classes.CartContainer}>
-      <BsXLg className={classes.CloseIcon} onClick={openCart} />
-      <h3 className={classes.CartTile}>Your Cart</h3>
       <div className={classes.CartItemsContainer}>
+        <BsXLg className={classes.CloseIcon} onClick={openCart} />
+        <h3 className={classes.CartTile}>Your Cart</h3>
         {cart.map((item) => (
           <CartItem cartItem={item} key={item.id} />
         ))}
+      </div>
+      <div className={classes.TotalAmountContainer}>
+        <h4 className={classes.totalAmountTitle}>Total Amount:</h4>
+        <button className={classes.totalAmountBtn}>Checkout</button>
+        <button className={classes.totalAmountBtn} onClick={emptyCart}>
+          Empty Cart
+        </button>
       </div>
     </div>
   );

c154a79abfbc44e97aec827a035c51955f553573
Petter <petterfogel96@gmail.com>
2022-02-07 18:54:51 +0100

    Renamed folder, file and component to Routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { FC, useContext } from "react";
+import { Routes } from "./components/layout/routes/Routes";
 import Header from "./components/layout/header/Header";
-import Pages from "./components/layout/pages/Pages";
-import "./App.css";
 import Context from "./context/Context";
 import SideCart from "./components/layout/cart/sideCart/SideCart";
+import "./App.css";
 
 const App: FC = () => {
   const { isCartOpen } = useContext(Context);
@@ -12,7 +12,7 @@ const App: FC = () => {
     <>
       <Header />
       <main>
-        <Pages />
+        <Routes />
         {isCartOpen && <SideCart />}
       </main>
     </>
--- src/components/layout/routes/Routes.tsx
+++ src/components/layout/routes/Routes.tsx
@@ -1,19 +1,17 @@
 import { FC } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes as Swtich } from "react-router-dom";
 import About from "../../../screens/about-screen/AboutScreen";
 import Home from "../../../screens/home-screen/HomeScreen";
 import Products from "../../../screens/products-screen/ProductsScreen";
 
-const Pages: FC = () => {
+export const Routes: FC = () => {
   return (
     <>
-      <Routes>
+      <Swtich>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/about" element={<About />} />
-      </Routes>
+      </Swtich>
     </>
   );
 };
-
-export default Pages;

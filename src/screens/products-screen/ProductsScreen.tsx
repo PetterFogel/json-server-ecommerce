1820d12298b61d3920333b29e8c7149622d469a8
PetterFogel <petterfogel96@gmail.com>
2022-02-27 21:15:39 +0100

    Implemented redux logic to api response states.

--- src/screens/products-screen/ProductsScreen.tsx
+++ src/screens/products-screen/ProductsScreen.tsx
@@ -1,14 +1,33 @@
-import { FC, useContext, useEffect } from "react";
+import axios from "axios";
+import { FC, useEffect } from "react";
 import { ProductsList } from "../../components/manage-products/products-list/ProductsList";
-import { Context } from "../../context/Context";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { fetchAllPosters, postersSlice } from "../../redux/posters/posterSlice";
+
 import classes from "./ProductsScreen.module.css";
 
 export const Products: FC = () => {
-  const { data, isLoading, error, sendRequest } = useContext(Context);
+  const dispatch = useAppDispatch();
+  const { posters, isLoading, error } = useAppSelector(fetchAllPosters);
+  const { setPosters, setIsLoading, setError } = postersSlice.actions;
 
   useEffect(() => {
-    sendRequest(`${process.env.REACT_APP_API_BASEURL}/posters`);
+    sendRequest();
   }, []);
+
+  const sendRequest = async () => {
+    try {
+      dispatch(setIsLoading(true));
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_BASEURL}/posters`
+      );
+      dispatch(setPosters(response.data));
+      dispatch(setIsLoading(false));
+    } catch (error) {
+      dispatch(setError(true));
+      dispatch(setIsLoading(false));
+    }
+  };
 
   if (error)
     return <p className={classes.error}>Oops! Something went wrong... :(</p>;
@@ -19,7 +38,7 @@ export const Products: FC = () => {
       {isLoading ? (
         <p className={classes.loading}>Loading...</p>
       ) : (
-        <ProductsList posters={data} />
+        <ProductsList posters={posters} />
       )}
     </section>
   );

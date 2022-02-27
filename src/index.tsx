f0670fd08e80c8b3f47b660ec3eb8457765dad8e
PetterFogel <petterfogel96@gmail.com>
2022-02-27 20:35:53 +0100

    Wrapped redux provider around app

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import { ContextProvider } from "./context/Context";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ContextProvider>
+      <Provider store={store}>
+        {/* <ContextProvider> */}
         <App />
-      </ContextProvider>
+        {/* </ContextProvider> */}
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")

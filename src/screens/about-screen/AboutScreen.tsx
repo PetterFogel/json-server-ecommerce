8ac5f2c13d55d0397fd26cb81310f325a77d0b93
PetterFogel <petterfogel96@gmail.com>
2022-03-05 10:39:28 +0100

    Added content for about + styling.

--- src/screens/about-screen/AboutScreen.tsx
+++ src/screens/about-screen/AboutScreen.tsx
@@ -1,5 +1,20 @@
 import { FC } from "react";
+import classes from "./AboutScreen.module.css";
 
 export const About: FC = () => {
-  return <div>About</div>;
+  return (
+    <section className={classes.section}>
+      <h2 className={classes.sectionTitle}>About</h2>
+      <p>
+        This is e-commerce project created with React, TypeScript and
+        json-server package to fetch and manage api calls with redux-toolkit.
+        <br />
+        <br />
+        Run `npm start` to run the application.
+        <br />
+        <br />
+        Run `npm run start-db` to start the json server.
+      </p>
+    </section>
+  );
 };
